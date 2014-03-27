4c58d7c65b3d9c5da0b00228c79dbd7e7a6eb509
chocoby <chocoby@gmail.com>
2014-03-27 15:04:33 +0900

    Add mappings

---
+++ custom_mappings.rb
@@ -0,0 +1,19 @@
+def custom_mappings
+  {
+    'wordpress' => %W(2010-04-16 wordpress-domain-after-setup),
+    'macbook-hdd-ssd-2' => %W(2010-04-18 macbook-hdd-ssd),
+    'google' => %W(2010-04-20 google-maps-miyazaki-streetview),
+    'html-helper-link' => %W(2010-06-09 cakephp-html-helper-link),
+    # remove: 2010-07-15-wordpress-3-0.md
+    'css-charset' => %W(2010-07-15 css-charset),
+    '231' => %W(2010-08-29 bicycle),
+    '250' => %W(2010-09-03 cycling-shoes-cleats),
+    '259' => %W(2010-09-04 saikoling-and-saddle),
+    '270' => %W(2010-09-05 saikoling-and-yacht),
+    '285' => %W(2010-09-11 saikoling),
+    '298' => %W(2010-09-19 hill-climb-okumusashi),
+    '298' => %W(2010-09-19 hill-climb-okumusashi),
+    '327' => %W(2010-10-02 arakawa-cycling-road),
+    '10-9' => %W(2010-10-09 saikoling),
+  }
+end
